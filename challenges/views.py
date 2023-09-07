8250968e1d26df1dc851dbdba4d1ab8844607fb8
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-07 11:54:49 +0100

    Added a /challenge route with links to all the challenges

--- challenges/views.py
+++ challenges/views.py
@@ -17,6 +17,18 @@ monthly_challenges = {
     "december": "december"
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -34,8 +46,9 @@ def monthly_challenge(request, month):
     month_text = None
     try:
         month_text = monthly_challenges[month]
-        return HttpResponse(f"{month_text} challenge")
+        response_data = f"<h1>{month_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
 
